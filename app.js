b44351ad3950a5ace9dda113f59f4fd8e2ec63e9
Adithya Kamath <adithyakamath@Adithyas-MacBook-Air.local>
2019-11-17 19:52:26 +0530

    Trying everything to add it to Heroku

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const dotenv = require('dotenv');
 const TelegramBot = require('node-telegram-bot-api');
+const express = require('express');
+const app = express();
+const port = process.env.PORT;
 dotenv.config({path: `${__dirname}/config/dev.env`});
 const token = process.env.TELEGRAM;
+console.log(process.env);
 
 const bot = new TelegramBot(token, {polling:true});
 const request = require('request');
-
-bot.onText(/\/weather (.+)/, (msg,match)=>{
+app.listen(port,()=>{
+  bot.onText(/\/weather (.+)/, (msg,match)=>{
     let city = match[1];
     let chatId = msg.chat.id;
 
@@ -23,3 +27,21 @@ bot.onText(/\/weather (.+)/, (msg,match)=>{
                 }
     });
 });
+});
+// bot.onText(/\/weather (.+)/, (msg,match)=>{
+//       let city = match[1];
+//       let chatId = msg.chat.id;
+  
+//       request(`http://kams-weather-api.herokuapp.com/${city}`,(error,response,body)=>{
+//           if(!error && body){
+//                   let res = JSON.parse(body);
+          
+//                   let celsiusTemp = ((res.temperature-32) * (5/9)).toFixed(2);
+//                     if(res.error){
+//                       bot.sendMessage(chatId,`Please enter an appropriate city!`);
+//                       return ;
+//                     }  
+//                       bot.sendMessage(chatId,`The current temperature for ${city} is ${celsiusTemp}°C. The summary of the weather: ${res.summary}`);
+//                   }
+//       });
+//   });
