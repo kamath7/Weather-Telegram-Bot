a14b62f2496712b8f8d6463c615a781ecab46493
Adithya Kamath <adithyakamath@Adithyas-MacBook-Air.local>
2019-11-17 20:33:52 +0530

    Cleaned it. Atleast now works on the local machine

--- app.js
+++ app.js
@@ -1,46 +1,12 @@
 const dotenv = require('dotenv');
 const TelegramBot = require('node-telegram-bot-api');
-const Agent = require('socks5-https-client/lib/Agent');
+
 dotenv.config({path: `${__dirname}/config/dev.env`});
 const token = process.env.TELEGRAM;
-const port = process.env.PORT||443;
-const host = process.env.HOST||'localhost';
-const extUrl = process.env.CUSTOM_ENV_VARIABLE || "http://telegram-kams-weather.herokuapp.com/";
 
-const bot = new TelegramBot(token,{webHook:{port:port, host: host}});
-bot.setWebHook(extUrl+`:${port}`+token);
-// const bot = new TelegramBot (token, {polling:true});
-// const bot = new TelegramBot(token, {
-//               polling:true,
-//               request: {
-//                 agentClass: Agent,
-//                 agentOptions: {
-//                   socksHost: process.env.PROXY_SOCKS5_HOST,
-//                   socksPort: process.env.PROXY_SOCKS5_PORT,
-//                   socksUsername: process.env.PROXY_SOCKS5_USERNAME,
-//                   socksPassword:process.env.PROXY_SOCKS5_PASSWORD
-//                 }
-//               }});
+const bot = new TelegramBot(token,{polling:true});
 const request = require('request');
-// app.listen(port,()=>{
-//   bot.onText(/\/weather (.+)/, (msg,match)=>{
-//     let city = match[1];
-//     let chatId = msg.chat.id;
 
-//     request(`http://kams-weather-api.herokuapp.com/${city}`,(error,response,body)=>{
-//         if(!error && body){
-//                 let res = JSON.parse(body);
-        
-//                 let celsiusTemp = ((res.temperature-32) * (5/9)).toFixed(2);
-//                   if(res.error){
-//                     bot.sendMessage(chatId,`Please enter an appropriate city!`);
-//                     return ;
-//                   }  
-//                     bot.sendMessage(chatId,`The current temperature for ${city} is ${celsiusTemp}°C. The summary of the weather: ${res.summary}`);
-//                 }
-//     });
-// });
-// });
 bot.onText(/\/weather (.+)/, (msg,match)=>{
       let city = match[1];
       let chatId = msg.chat.id;
